899706ab7901a7ef0f42601a29764e89aa3ed545
Cesar Freitas <cesarfreitascontato@gmail.com>
2022-08-11 12:17:27 -0300

    add JWTAuthenticationFilter

--- src/main/kotlin/br/com/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/forum/config/SecurityConfiguration.kt
@@ -1,8 +1,10 @@
 package br.com.forum.config
 
+import br.com.forum.security.JWTAuthenticationFilter
 import br.com.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -12,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
@@ -25,15 +28,13 @@ class SecurityConfiguration(
         http?.
         authorizeRequests()?.
         //antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
-        antMatchers("/login")?.permitAll()?.
+        antMatchers(HttpMethod.POST, "/login")?.permitAll()?.
         anyRequest()?.
         authenticated()?.
         and()
         http?.addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
-        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
-        and()?.
-        formLogin()?.disable()?.
-        httpBasic()
+        http?.addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), OncePerRequestFilter::class.java)
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
     @Bean
