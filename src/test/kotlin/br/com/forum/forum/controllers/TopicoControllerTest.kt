01963829e6a5425209818fcf42607d0dff51aaa6
Cesar Freitas <cesarfreitascontato@gmail.com>
2022-08-17 20:03:25 -0300

    primeiro teste implementado para quando chamar topico sem um token

--- src/test/kotlin/br/com/forum/forum/controllers/TopicoControllerTest.kt
+++ src/test/kotlin/br/com/forum/forum/controllers/TopicoControllerTest.kt
@@ -1,10 +1,12 @@
 package br.com.forum.forum.controllers
 
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
@@ -17,12 +19,20 @@ class TopicoControllerTest {
 
     private lateinit var mockMvc: MockMvc
 
+    companion object {
+        private const val RECURSO = "/topicos/"
+    }
+
     @BeforeEach
     fun setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
             .apply<DefaultMockMvcBuilder?>(
                 SecurityMockMvcConfigurers.springSecurity()
             ).build()
+    }
 
+    @Test
+    fun `deve retornar codigo 400 quando chamar topico sem token`(){
+        mockMvc.get(RECURSO).andExpect { status { is4xxClientError() } }
     }
 }
