fbb513b471d13fcacbc03ff847f017c603364f36
Cesar Freitas <cesarfreitascontato@gmail.com>
2022-08-23 19:38:21 -0300

    dependencia thymeleaf adicionada no projeto para gerar relatorio personalizado em html

--- src/main/kotlin/br/com/forum/controllers/RelatorioController.kt
+++ src/main/kotlin/br/com/forum/controllers/RelatorioController.kt
@@ -1,16 +1,20 @@
 package br.com.forum.controllers
 
 import br.com.forum.services.TopicoService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("relatorios")
 class RelatorioController(
     private val topicoService: TopicoService
 ) {
 
     @GetMapping
-    fun relatorio() = topicoService.relatorio()
+    fun relatorio(model: Model): String {
+        model.addAttribute("topicosPorCategorias", topicoService.relatorio())
+        return "relatorio"
+    }
 }
